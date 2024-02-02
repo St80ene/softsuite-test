93463cc22e1f9b932cca83e7f81c22e10263afe6
etiene <etienejames5@gmail.com>
2024-02-02 10:49:46 +0100

    Fix form refresh
    Signed-off-by: etiene <etienejames5@gmail.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,6 @@
 import {
   useGetDepartmentQuery,
   useGetElementLookupsCategoryAndValueQuery,
-  useGetLookupsQuery,
   useGetSubOrganizationQuery,
   usePostElementMutation,
 } from '../redux/dataSlice';
